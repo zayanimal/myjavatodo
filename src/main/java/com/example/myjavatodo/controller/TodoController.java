f303b1c54c3fc29d2ac68553c5307a4804b4277e
Vladimir Zaychikov <zaychikov.vl@gmail.com>
2021-11-29 23:26:12 +0300

    todo controller, service

--- src/main/java/com/example/myjavatodo/controller/TodoController.java
+++ src/main/java/com/example/myjavatodo/controller/TodoController.java
@@ -1,17 +1,21 @@
 package com.example.myjavatodo.controller;
 
 import com.example.myjavatodo.entity.TodoEntity;
+import com.example.myjavatodo.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/todos")
 public class TodoController {
+    @Autowired
+    private TodoService todoService;
 
     @PostMapping
-    public ResponseEntity createTodo(@RequestBody TodoEntity todo) {
+    public ResponseEntity createTodo(@RequestBody TodoEntity todo, @RequestParam Long userId) {
         try {
-
+            return ResponseEntity.ok(todoService.createTodo(todo, userId));
         } catch (Exception e) {
             return ResponseEntity.badRequest().body("Произошла ошибка");
         }
@@ -20,7 +24,7 @@ public class TodoController {
     @PutMapping
     public ResponseEntity completeTodo(@RequestParam Long id) {
         try {
-
+            return ResponseEntity.ok(todoService.completeTodo(id));
         } catch (Exception e) {
             return ResponseEntity.badRequest().body("Произошла ошибка");
         }
--- src/main/java/com/example/myjavatodo/service/TodoService.java
+++ src/main/java/com/example/myjavatodo/service/TodoService.java
@@ -1,6 +1,9 @@
 package com.example.myjavatodo.service;
 
+import com.example.myjavatodo.entity.TodoEntity;
+import com.example.myjavatodo.entity.UserEntity;
 import com.example.myjavatodo.repository.TodoRepo;
+import com.example.myjavatodo.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +11,21 @@ import org.springframework.stereotype.Service;
 public class TodoService {
     @Autowired
     private TodoRepo todoRepo;
+
+    @Autowired
+    private UserRepo userRepo;
+
+    public TodoEntity createTodo(TodoEntity todo, Long userId) {
+        UserEntity user = userRepo.findById(userId).get();
+        todo.setUser(user);
+
+        return todoRepo.save(todo);
+    }
+
+    public TodoEntity completeTodo(Long id) {
+        TodoEntity todo = todoRepo.findById(id).get();
+        todo.setCompleted(!todo.getCompleted());
+
+        return todoRepo.save(todo);
+    }
 }
