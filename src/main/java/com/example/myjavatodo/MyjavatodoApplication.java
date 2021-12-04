20589f02ed94847bc410defe682c9ae780b19d07
Vladimir Zaychikov <zaychikov.vl@gmail.com>
2021-12-04 15:13:07 +0300

    авторизация на mvc

--- src/main/java/com/example/myjavatodo/MyjavatodoApplication.java
+++ src/main/java/com/example/myjavatodo/MyjavatodoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyjavatodoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Throwable {
 		SpringApplication.run(MyjavatodoApplication.class, args);
 	}
 
